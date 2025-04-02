e04520fe8d0e96c0f74f8eeb42423e46c03b3981
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-04-02 11:14:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+	let h1 = document.createElement("h1");
+window.addEventListener("resize",(e)=>{
+	h1.innerText =`width ${window.innerWidth } and Height ${window.innerHeight}`;
+	
+	document.querySelector("#sizeInfo").appendChild(h1);
+})
